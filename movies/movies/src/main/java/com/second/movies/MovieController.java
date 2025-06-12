109d21d4c37a61e2d621d73361cc788d8f2093b1
Shashwath-DU <shashwath18.du2gmail.com>
2025-06-12 20:01:00 +0530

    Added Spring Boot backend to repo

---
+++ movies/movies/src/main/java/com/second/movies/MovieController.java
@@ -0,0 +1,43 @@
+package com.second.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getaAlMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Movie> getSingleMovie(@PathVariable String id) {
+//        try {
+//            ObjectId objectId = new ObjectId(id);
+//            return movieService.singleMovie(objectId)
+//                .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
+//                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//        } catch (IllegalArgumentException e) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);  // Invalid ObjectId
+//        }
+//    }
+
+
+}
---
+++ movies/movies/src/main/java/com/second/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.second.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
